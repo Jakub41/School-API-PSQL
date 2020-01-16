9beb845d97b962a043dc6594bdea591f9aad4250
jakub41 <lemiszewski@gmx.com>
2020-01-16 16:51:32 +0100

    Migrations

---
+++ src/database/migrations/20200116151826-Student.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Students", {
+            _id: {
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                autoIncrement: true
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            email: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+                unique: true
+            },
+            dateOfBirth: {
+                type: Sequelize.DATEONLY,
+                allowNull: false
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false
+            }
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("Students");
+    }
+};
---
+++ src/database/migrations/20200116152458-Project.js
@@ -0,0 +1,52 @@
+"use strict";
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Projects", {
+            project_id: {
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                autoIncrement: true
+            },
+            student_id: {
+                foreignKey: true,
+                type: Sequelize.UUIDV4,
+                allowNull: false
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true
+            },
+            description: {
+                type: Sequelize.TEXT,
+                allowNull: true
+            },
+            creationDate: {
+                type: Sequelize.DATEONLY,
+                allowNull: true
+            },
+            repoUrl: {
+                type: Sequelize.TEXT,
+                allowNull: true
+            },
+            liveUrl: {
+                type: Sequelize.TEXT,
+                allowNull: true
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false
+            }
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("Projects");
+    }
+};
